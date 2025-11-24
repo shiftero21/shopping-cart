5cb9bde015880c8d8925532b2f9233b943145392
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-11-24 19:45:35 -0300

    Add ErrorPage

--- src/app/routes/ErrorPage/ErrorPage.jsx
+++ src/app/routes/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router";
+
 const ErrorPage = () => {
-  return <></>;
+  return (
+    <>
+      (
+      <div>
+        <h1>Oh no, this route doesn't exist!</h1>
+        <Link to="/">
+          You can go back to the home page by clicking here, though!
+        </Link>
+      </div>
+      );
+    </>
+  );
 };
 
 export default ErrorPage;
