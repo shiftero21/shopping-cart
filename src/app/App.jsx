551db5b0c8996fc3b194d4780202512dd70e717e
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-11-24 14:04:10 -0300

    Add reset css and dummy folders

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Mi shopping cart</h1>
+      <hr></hr>
+      <h2>Probando cositas</h2>
+      <p>lalaldas ajshd ajskdh asjd hasjhaskdsahjdaskdhas kjashdkhk</p>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./reset.css";
 import App from "./app/App";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
