342397b48e133b79e1899ff275cb1fd66c71958b
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-11-25 19:12:30 -0300

    Change style Home route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,15 @@
-import "./App.css";
 import NavigationBar from "../components/Navigation/NavBar";
 import { Outlet } from "react-router";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <h1>Mi shopping cart</h1>
       <NavigationBar></NavigationBar>
 
       <Outlet></Outlet>
-    </>
+    </div>
   );
 }
 
--- src/app/routes/Home/Home.jsx
+++ src/app/routes/Home/Home.jsx
@@ -1,10 +1,35 @@
+import styles from "./Home.module.css";
+
 const HomePage = () => {
   return (
-    <>
-      <h1>Hello from Home page!</h1>
-      <p>So, how are you?</p>
-      <hr />
-    </>
+    <div className={styles.wrapper}>
+      <div className={styles.wrapperTitulos}>
+        <div
+          id="fast"
+          className={`${styles.titulos} ${styles["titulos--fast"]}`}
+        >
+          Fast
+        </div>
+        <div
+          id="charge"
+          className={`${styles.titulos} ${styles["titulos--charge"]}`}
+        >
+          Charge
+        </div>
+        <div
+          id="portability"
+          className={`${styles.titulos} ${styles["titulos--portab"]}`}
+        >
+          Portability!
+        </div>
+      </div>
+      <div className={styles.wrapperHeader}>
+        <div className={styles.header}>
+          <h1>POWERBANKS</h1>
+        </div>
+        <div className={styles.headerImage}></div>
+      </div>
+    </div>
   );
 };
 
