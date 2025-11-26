0f9259192bb1dc28fc69e97a51fd6cccf1c34836
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-11-26 18:52:53 -0300

    Change navbar position

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,15 @@
-import NavigationBar from "../components/Navigation/NavBar";
 import { Outlet } from "react-router";
 import styles from "./App.module.css";
 import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import NavigationBar from "../components/Navigation/NavBar";
+import ScrollToTop from "../components/ScrollToTop";
 
 function App() {
   return (
     <div className={styles.wrapper}>
-      <h1>Mi shopping cart</h1>
+      <ScrollToTop></ScrollToTop>
+      <Header></Header>
       <NavigationBar></NavigationBar>
 
       <Outlet></Outlet>
