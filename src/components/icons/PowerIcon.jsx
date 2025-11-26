49c9a7a2d997969971a609d9addc3506108e7bdb
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-11-26 18:51:13 -0300

    Add powerIcon logo

---
+++ src/components/icons/PowerIcon.jsx
@@ -0,0 +1,17 @@
+const PowerIcon = ({ size = 32 }) => {
+  return (
+    <svg
+      fill="#000000"
+      width={size}
+      height={size}
+      viewBox="0 0 32 32"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>power</title>
+      <path d="M26 23h-6v5h-8v-5h-6v-12h4v-8h4v8h4v-8h4v8h4v12z" />
+    </svg>
+  );
+};
+
+export default PowerIcon;
