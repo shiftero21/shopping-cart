69233a7ceacd4d34d4303208cef54fe9ffb66db9
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-11-28 13:44:31 -0300

    Add Cart dummy content

--- src/app/routes/Cart/Cart.jsx
+++ src/app/routes/Cart/Cart.jsx
@@ -1,5 +1,162 @@
 const Cart = () => {
-  return <></>;
+  return (
+    <>
+      <h1>adasdas d ASDAS </h1>
+      <p>asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuilhuiu</p>
+      <p>asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuilhuiu</p>
+
+      <p>asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuilhuiu</p>
+
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+      <p>
+        asdasdas aasdas asd as das asdasdjkashdjk asjhjhjshdfuil. AslkdfAslkdf
+        Aslkdf Aslkdf Aslkdf Aslkdf Aslkdf huiu
+      </p>
+    </>
+  );
 };
 
 export default Cart;
