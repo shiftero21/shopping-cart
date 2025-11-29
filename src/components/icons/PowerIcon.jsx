6a0ea128c288f009e7c8aea858bd89e4cad9a8e9
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-11-29 18:35:40 -0300

    Change colors hover

--- src/components/icons/PowerIcon.jsx
+++ src/components/icons/PowerIcon.jsx
@@ -8,7 +8,7 @@ const PowerIcon = ({ size = 32 }) => {
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <title>power</title>
+      {/* <title>power</title> */}
       <path d="M26 23h-6v5h-8v-5h-6v-12h4v-8h4v8h4v-8h4v8h4v12z" />
     </svg>
   );
