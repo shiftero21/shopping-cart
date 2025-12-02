96218b623aad0e3dc2ea8e43a8711ac7e47550e7
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-12-02 16:04:48 -0300

    Add Tooltip Component

---
+++ src/components/Tooltip/Tooltip.jsx
@@ -0,0 +1,23 @@
+import styles from "./Tooltip.module.css";
+
+const Tooltip = () => {
+  return (
+    <div classNameName={styles.wrapper}>
+      <div className={`${styles.tooltip} ${styles.tooltip3}`}>
+        <div className={styles.pin}></div>
+        <div className={styles.tooltipContent}>
+          {/* <div className="arrow"></div> */}
+          <div className={styles.content}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus molestiae accusantium magni ex numquam eveniet,
+              doloremque dolores impedit beatae quas!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tooltip;
