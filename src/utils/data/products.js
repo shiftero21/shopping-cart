4c0423c5a98a89b7363015afc5e77acccfc93a77
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-12-03 23:38:07 -0300

    Add products for Shop Page

--- src/utils/data/products.js
+++ src/utils/data/products.js
@@ -4,6 +4,11 @@ import carg01 from "../../assets/images/cargador-portatil-01.jpg";
 import carg02 from "../../assets/images/cargador-portatil-02.jpg";
 import carg03 from "../../assets/images/cargador-portatil-03.jpg";
 import carg04 from "../../assets/images/cargador-portatil-04.jpg";
+import carg05 from "../../assets/images/cargador-portatil-05.jpg";
+import carg06 from "../../assets/images/cargador-portatil-06.jpg";
+import carg07 from "../../assets/images/cargador-portatil-07.jpg";
+import carg08 from "../../assets/images/cargador-portatil-08.jpg";
+import carg09 from "../../assets/images/crave.webp";
 
 const min = 4.99;
 const max = 24.99;
@@ -43,6 +48,56 @@ const products = [
     name: "Cargador portatil 04",
     price: Number((Math.random() * (max - min) + min).toFixed(2)),
     src: carg04,
+    description: "Cargador Portatil 8500 Mah Con Cable Mixio Mp-10",
+    category: "tech",
+  },
+
+  {
+    id: uniqid("prod-"),
+    slug: "cargador-portatil-05",
+    name: "Cargador portatil 05",
+    price: Number((Math.random() * (max - min) + min).toFixed(2)),
+    src: carg05,
+    description: "Cargador Portatil 8500 Mah Con Cable Mixio Mp-10",
+    category: "tech",
+  },
+
+  {
+    id: uniqid("prod-"),
+    slug: "cargador-portatil-06",
+    name: "Cargador portatil 06",
+    price: Number((Math.random() * (max - min) + min).toFixed(2)),
+    src: carg06,
+    description: "Cargador Portatil 8500 Mah Con Cable Mixio Mp-10",
+    category: "tech",
+  },
+
+  {
+    id: uniqid("prod-"),
+    slug: "cargador-portatil-07",
+    name: "Cargador portatil 07",
+    price: Number((Math.random() * (max - min) + min).toFixed(2)),
+    src: carg07,
+    description: "Cargador Portatil 8500 Mah Con Cable Mixio Mp-10",
+    category: "tech",
+  },
+
+  {
+    id: uniqid("prod-"),
+    slug: "cargador-portatil-08",
+    name: "Cargador portatil 08",
+    price: Number((Math.random() * (max - min) + min).toFixed(2)),
+    src: carg08,
+    description: "Cargador Portatil 8500 Mah Con Cable Mixio Mp-10",
+    category: "tech",
+  },
+
+  {
+    id: uniqid("prod-"),
+    slug: "cargador-portatil-09",
+    name: "Cargador portatil 09",
+    price: Number((Math.random() * (max - min) + min).toFixed(2)),
+    src: carg09,
     description: "Cargador Portatil 8500 Mah Con Cable Mixio Mp-10",
     category: "tech",
   },
