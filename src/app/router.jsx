42e02acb508b75b216f303cb35efcb1f455608b3
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-12-03 21:18:34 -0300

    Fix vulnerability react version

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -3,7 +3,6 @@ import HomePage from "./routes/Home/Home";
 import Cart from "./routes/Cart/Cart";
 import Shop from "./routes/Shop/Shop";
 import ErrorPage from "./routes/ErrorPage/ErrorPage";
-// import ProductDetailsPage from "../routes/Shop/ProductDetailsPage";
 import ProductDetailsPage from "./routes/Shop/ProductDetailsPage";
 
 const routes = [
