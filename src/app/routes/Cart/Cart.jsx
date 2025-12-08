eaceba32b597408b5f9c6456f09260a6fdbada65
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-12-08 17:39:02 -0300

    Add Back navigation link to Cart

--- src/app/routes/Cart/Cart.jsx
+++ src/app/routes/Cart/Cart.jsx
@@ -1,4 +1,6 @@
 import { useOutletContext } from "react-router-dom";
+import { Link } from "react-router-dom";
+import BackIcon from "../../../components/icons/BackIcon";
 
 function Cart() {
   // 1. Obtener el contexto. Es crucial que useOutletContext() devuelva un objeto
@@ -11,6 +13,13 @@ function Cart() {
   if (cart.length === 0) {
     return (
       <div className="flex flex-col items-center justify-center p-8 min-h-[60vh] bg-gray-50">
+        <Link
+          to="/shopping"
+          className="text-center text-indigo-600 hover:text-indigo-800 font-medium mt-2"
+        >
+          <BackIcon></BackIcon>
+          Seguir Comprando
+        </Link>
         <h1 className="text-3xl font-bold text-gray-700 mb-4">
           Tu carrito está vacío 😔
         </h1>
@@ -32,6 +41,12 @@ function Cart() {
 
   return (
     <div className="max-w-4xl mx-auto p-4 sm:p-8 bg-white shadow-lg rounded-xl my-10">
+      <Link
+        to="/shopping"
+        className="text-center text-indigo-600 hover:text-indigo-800 font-medium mt-2"
+      >
+        Seguir Comprando
+      </Link>
       <h2 className="text-4xl font-extrabold text-indigo-700 mb-8 border-b pb-3">
         Cesta de Compras
       </h2>
