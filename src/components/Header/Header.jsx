d5bf0527a064bb0acaa333d7ef337373cfe7e7ff
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2025-12-16 20:11:26 -0300

    Change font-size Header and apply BEM

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,14 @@
 import styles from "./Header.module.css";
 import PowerIcon from "../icons/PowerIcon";
-import NavigationBar from "../Navigation/NavBar";
-import useScrollDirection from "../../hooks/useScrollDirection";
 
 const Header = () => {
-  const scrollDirection = useScrollDirection();
-  const navBarClass =
-    scrollDirection === "down" ? "navbar--hidden" : "navbar--visible";
   return (
     <header className={styles.header}>
-      <div className={styles.wrapper}>
-        <div className={styles.main}>
-          <PowerIcon size={48}></PowerIcon>
-          <div className={styles.header__logo}>
-            <h1>Power Lord</h1>
-          </div>
+      <div className={styles.header__main}>
+        <PowerIcon size={48}></PowerIcon>
+        <div className={styles.header__logo}>
+          <h1>Power Lord</h1>
         </div>
-        {/* <div className={styles.wrapperNav}> */}
-        {/* <NavigationBar navBarClass={navBarClass}></NavigationBar> */}
-        {/* </div> */}
-
-        {/* <NavigationBar></NavigationBar> */}
       </div>
     </header>
   );
