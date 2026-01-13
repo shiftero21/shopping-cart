90eeb42e02c04bb23caa1d4d9590ae4a3a10addb
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2026-01-13 19:25:31 -0300

    Add added button in ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -10,6 +10,7 @@ const ProductCard = ({ product }) => {
   const translatedDescription = t(`products.${slug}.description`);
 
   const { cart, addToCart } = useOutletContext();
+  const isAdded = cart.some((item) => item.id === id);
   return (
     <div className={styles.card}>
       <h1 className="mb-2">{t("productCard.new")}</h1>
@@ -30,13 +31,20 @@ const ProductCard = ({ product }) => {
           <span className={styles.card__price}>
             {t("productCard.price")}: {price}
           </span>
-          <AddToCartBtn
-            product={product}
-            addToCart={addToCart}
-            className="w-50 py-2 px-2 bg-indigo-600 text-white font-semibold rounded-xl shadow-lg hover:bg-indigo-700 transition duration-800 transform  active:outline-none active:ring-4 active:ring-indigo-300"
-          >
-            {t("productCard.textAdd")}
-          </AddToCartBtn>
+
+          {isAdded ? (
+            <button className="w-50 py-2 px-2 bg-green-600 text-white font-semibold rounded-xl">
+              {t("productCard.added")}
+            </button>
+          ) : (
+            <AddToCartBtn
+              product={product}
+              addToCart={addToCart}
+              className="w-50 py-2 px-2 bg-indigo-600 text-white font-semibold rounded-xl shadow-lg hover:bg-indigo-700 transition duration-800 transform active:outline-none active:ring-4 active:ring-indigo-300"
+            >
+              {t("productCard.textAdd")}
+            </AddToCartBtn>
+          )}
         </div>
       </div>
     </div>
