d037daf54a2c50d487547ed4a57913dd6fe43811
shiftero21 <117830510+shiftero21@users.noreply.github.com>
2026-01-14 14:59:39 -0300

    Add web analytics vercel

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./app/router";
 import { Suspense } from "react";
 import i18n from "./i18next";
+import { Analytics } from "@vercel/analytics/next";
 
 const router = createBrowserRouter(routes);
 
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Suspense fallback={<div>Loading language...</div>}>
       <RouterProvider router={router}></RouterProvider>
+      <Analytics />
     </Suspense>
   </StrictMode>
 );
